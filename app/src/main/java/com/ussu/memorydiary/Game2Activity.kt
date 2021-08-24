af66d638745f240184bbafe7bfff4c1e92245e40
ZSU <zsueue@gmail.com>
2021-08-24 22:47:48 +0900

    v2.0

--- app/src/main/java/com/ussu/memorydiary/Game2Activity.kt
+++ app/src/main/java/com/ussu/memorydiary/Game2Activity.kt
@@ -60,34 +60,41 @@ class Game2Activity : AppCompatActivity() {
 
                     if (response.body()!!.answer == "99") {
                         gameTextWarning.isVisible = true
-
-                        val BASE_URL = "http://192.168.0.104:8080"
                         var game_text = response.body()!!.game_text
                         GameTextTextView.text = "$game_text"
+
                         var answer = AnswerEditText.text.toString()
 
-                        var gson = GsonBuilder()
-                            .setLenient()
-                            .create()
+                        if (game_text.contains(answer)) {
+                            val BASE_URL = "http://192.168.0.104:8080"
+                            var answer = AnswerEditText.text.toString()
 
-                        val retrofit = Retrofit.Builder()
-                            .baseUrl(BASE_URL)
-                            .addConverterFactory(GsonConverterFactory.create(gson))
-                            .build()
+                            var gson = GsonBuilder()
+                                .setLenient()
+                                .create()
 
-                        val api = retrofit.create(diaryAPI::class.java)
-                        val callGetGameText = api.getGameText((gameText(answer)))
+                            val retrofit = Retrofit.Builder()
+                                .baseUrl(BASE_URL)
+                                .addConverterFactory(GsonConverterFactory.create(gson))
+                                .build()
 
-                        callGetGameText.enqueue(object : Callback<gameText> {
-                            override fun onResponse(call: Call<gameText>, response: Response<gameText>) {
-                                Toast.makeText(this@Game2Activity, "보내주신 데이터가 잘 저장되었어요!", Toast.LENGTH_LONG).show()
-                                Log.d(ContentValues.TAG, "성공: ${response.raw()}")
-                            }
+                            val api = retrofit.create(diaryAPI::class.java)
+                            val callGetGameText = api.getGameText((gameText(answer)))
 
-                            override fun onFailure(call: Call<gameText>, t: Throwable) {
-                                Log.d(ContentValues.TAG, "실패: $t")
-                            }
-                        })
+                            callGetGameText.enqueue(object : Callback<gameText> {
+                                override fun onResponse(call: Call<gameText>, response: Response<gameText>) {
+                                    Toast.makeText(this@Game2Activity, "보내주신 데이터가 잘 저장되었어요!", Toast.LENGTH_LONG).show()
+                                    Log.d(ContentValues.TAG, "성공: ${response.raw()}")
+                                }
+
+                                override fun onFailure(call: Call<gameText>, t: Throwable) {
+                                    Log.d(ContentValues.TAG, "실패: $t")
+                                }
+                            })
+                        }
+                        else {
+                            Toast.makeText(this@Game2Activity, "일치하는 단어가 없습니다. 다시 입력해주세요.", Toast.LENGTH_LONG).show()
+                        }
                     }
                     btnAnswer.setOnClickListener {
                         //답 입력받기

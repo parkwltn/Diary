fbd170330f386884203c892e0ddbf2212cb66d0a
ZSU <zsueue@gmail.com>
2021-10-10 01:53:37 +0900

    v6.8

--- app/src/main/java/com/ussu/memorydiary/AgreeActivity.kt
+++ app/src/main/java/com/ussu/memorydiary/AgreeActivity.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
 
 class AgreeActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agree)
+
     }
 
-    fun clickLogin(view: View) {
+    fun onCheckboxClicked(view: View) {
+
         var intent = Intent(this@AgreeActivity, LoginActivity::class.java)
         startActivity(intent)
+
     }
 }
